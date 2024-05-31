6ac9dbfd6a7c629eb23793a4de5a64706b42e9bc
unknown <ibrahimabila.diop@adie.sn>
2024-05-31 00:20:22 +0000

    model et controlleur pour les sondages - controlleur utilisateurs - view show

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SondageController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +21,8 @@ Route::get('/signup', [AuthController::class, 'signup'])->name('auth.signup');
 Route::post('/signup', [AuthController::class, 'dosignup'])->name('auth.dosignup');
 Route::get('/signin', [AuthController::class, 'signin'])->name('auth.signin');
 Route::post('/signin', [AuthController::class, 'dosignin'])->name('auth.dosignin');
-Route::get('/profil', [AuthController::class, 'profil'])->name('auth.profil');
+Route::get('/profil', [AuthController::class, 'profil'])->name('auth.profil')->middleware('auth');
+Route::get('/show', [UsersController::class, 'show'])->name('show')->middleware('auth');
 
 Route::get('/', function () {
     return view('welcome');
